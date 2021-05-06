eece169bbe86a1dc07d680b00076e032a253e305
Carlos Castillo <castillo25ca@gmail.com>
2021-05-06 12:20:30 -0700

    update to header

--- myportfolio/src/components/Header.js
+++ myportfolio/src/components/Header.js
@@ -7,7 +7,6 @@ class Header extends Component {
 
     render() {
         return (<div>
-            <h1 className='heading-background'>C NEILL</h1>
             <header>
                 <h1>
                     {data.name}
@@ -16,9 +15,6 @@ class Header extends Component {
             <p className='header-title'>
                 {data.headerTagline[0]}<br></br>{data.headerTagline[1]}<br></br>
                 {data.headerTagline[2]}
-                <br></br>
-                <button><a href={`mailto:${data.contactEmail}`} rel="noopener noreferrer" >Contact</a></button>
-                <button><a href={`${data.resume}`} rel="noopener noreferrer" >Resume</a></button>
             </p>
 
 
