a223337860a9fce748ad56da32899ec0d9c3529c
Carlos Castillo <castillo25ca@gmail.com>
2021-05-05 22:25:22 -0700

    data.js fix

---
+++ myportfolio/src/data.js
@@ -0,0 +1,75 @@
+export default
+    {
+
+        name: 'CARLOS CASTILLO',
+        headerTagline: [
+            'DONT FOLLOW THE PACK..',
+            'LEAD IT!!!',
+        ],
+        contactEmail: 'ccastillo25@live.com',
+        abouttext: "My name is Carlos Castillo. I'm 28 years old, I was born in Los Angeles, California. I love to work out and watch sports,baseball being my favorite sport of all with the Dodgers being my favorite team I am currently a student at UCSD extension for coding, on my journey to begin a career as a web developer. I have always been driven by my competitive spirit, and always wanting to be the best, and I will strive everyday to become the very best web developer I know I can be.",
+        aboutImage: './myImage.jpg',
+        ShowAboutImage: true,
+
+        projects: [
+            {
+                id: 1,
+                title: 'Design A Date',
+                service: 'A program to make dating easier',
+                imageSrc: "./design.png",
+                url: 'https://christopherneill.github.io/GroupProject/'
+            },
+            {
+                id: 2,
+                title: 'Find a Lost pet',
+                service: 'microchip matcher',
+                imageSrc: "./catsndogs.png",
+                url: 'https://polar-stream-66961.herokuapp.com/'
+            },
+
+            {
+                id: 3,
+                title: 'Password Generator',
+                service: 'First use of javascript',
+                imageSrc: "./pass.png",
+                url: 'https://christopherneill.github.io/javascript/'
+            },
+            {
+                id: 4,
+                title: 'Command line inteface',
+                service: 'Node Program Development',
+                imageSrc: "./rmgen.png",
+                url: 'https://github.com/ChristopherNeill/clirmgen'
+            },
+            {
+                id: 5,
+                title: 'Eat da burger',
+                service: 'MySQL, Node, Express, Handlebars and a ORM',
+                imageSrc: "./burger.png",
+                url: 'https://burgermonster123.herokuapp.com/'
+            },
+            {
+                id: 6,
+                title: 'Weather App',
+                service: 'Api implementation',
+                imageSrc: "./weather.png",
+                url: 'https://christopherneill.github.io/weatherapp2/'
+            }
+
+
+
+
+        ],
+        social: [
+
+            {
+                name: 'Github',
+                url: 'https://github.com/ccastillo25'
+            },
+            {
+                name: 'Linkedin',
+                url: 'https://www.linkedin.com/in/carlos-castillo-0052a2a5'
+            },
+
+        ]
+    }
